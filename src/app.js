c45ebd3ef322c8ceb6db6d973c412b4ea5b3e661
CamiloCastellanosc <camiloalejo89@hotmail.com>
2023-09-06 18:22:21 -0500

    feat: Proyecto final kata 5

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express  from "express";
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js'
+import serviceRouter from './routes/service.routes.js'
+import companyRouter from './routes/company.routes.js'
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+// routes
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/services', serviceRouter);
+app.use('/api/companies', companyRouter);
+
+
+
+export default app;
